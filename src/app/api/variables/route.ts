04b6a0906793cfc164ce28ca1077c0b159f6f543
lexrojas <legalcr.rojas@gmail.com>
2025-08-28 15:31:32 -0600

    ddd fefewf

--- src/app/api/variables/route.ts
+++ src/app/api/variables/route.ts
@@ -4,7 +4,11 @@ import { createHandler, listHandler } from "@/lib/crud";
 export async function GET(req: NextRequest) {
   return listHandler(req, "variables", { idVariable: "desc" });
 }
-
+/**
+ *
+ * @param req
+ * @returns la tabla ya creada.
+ */
 export async function POST(req: NextRequest) {
   return createHandler(req, "variables");
 }
