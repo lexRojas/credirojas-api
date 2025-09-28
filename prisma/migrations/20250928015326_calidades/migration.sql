37819ea68da62c38dca6fe98d2b4666400954988
lexrojas <legalcr.rojas@gmail.com>
2025-09-27 20:09:09 -0600

    cambios_db_socio

---
+++ prisma/migrations/20250928015326_calidades/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `socio` ADD COLUMN `direccion` VARCHAR(191) NULL,
+    ADD COLUMN `estado_civil` VARCHAR(191) NULL;
---
+++ prisma/migrations/20250928020716_calidades_2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `socio` ADD COLUMN `profesion` VARCHAR(191) NULL;
